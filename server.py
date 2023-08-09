99cc7411623ea33c274d8d70c66db67749ff0e76
6ocra3 <6o3kmm@mail.ru>
2023-08-09 22:53:09 +0300

    test add_url_rule

--- server.py
+++ server.py
@@ -1,11 +1,8 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
+def hello_world():
+    return {"id": 4}, 200
 from flask import Flask
 
 app = Flask(__name__)
+app.add_url_rule("/", view_func=hello_world)
 
-@app.route('/')
-def hello_world():
-    return {"id": 4}, 200
 
