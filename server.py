b9abcb03d21480dd03be8b4f5b9ed980c81ede8f
6ocra3 <6o3kmm@mail.ru>
2023-08-09 23:48:17 +0300

    import con 2

--- server.py
+++ server.py
@@ -8,45 +8,7 @@ import argparse
 # from flask_cors import CORS
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-# from DB.client.client import MySQLConnection
-class MySQLConnection:
-    def __init__(self, host, user, password, db_name, rebuild_db=False):
-        self.host = host
-        # self.port = port
-        self.user = user
-        self.password = password
-        self.db_name = db_name
-        self.rebuild_db = rebuild_db
-        self.connection = self.connect()
-        session = sessionmaker(
-            bind=self.connection.engine,
-            autocommit=True,
-            autoflush=True,
-            enable_baked_queries=False,
-            expire_on_commit=True        
-        )
-
-        self.session = session()
-
-
-
-    def get_connection(self, db_created=False):
-        engine = sqlalchemy.create_engine(
-            f"mysql+pymysql://{self.user}:{self.password}@{self.host}/{self.db_name if db_created else ''}"
-        )
-        return engine.connect()
-    
-    def connect(self):
-        connection = self.get_connection()
-        if self.rebuild_db:
-            print(1)
-            connection.execute(f"DROP DATABASE IF EXISTS {self.db_name}")
-            connection.execute(f"CREATE DATABASE {self.db_name}")
-        return self.get_connection(db_created=True)
-    
-    def execute_query(self, query):
-        res = self.connection.execute(query)
-        return res
+from DB.client.client import MySQLConnection
     
 class DbInteraction:
     def __init__(self, host, user, password, db_name, rebuild_db=False):
