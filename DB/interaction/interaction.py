30d327b404b12435c6c2fc0b5f2981345801cbc0
6ocra3 <6o3kmm@mail.ru>
2023-08-10 00:08:44 +0300

    add all i have

--- DB/interaction/interaction.py
+++ DB/interaction/interaction.py
@@ -4,7 +4,7 @@ import copy
 
 from DB.client.client import MySQLConnection
 from DB.models.models import Base, User, Weeks, Tasks
-
+from DB.exceptions import UserNotFoundException
 
 
 
@@ -38,3 +38,75 @@ class DbInteraction:
         else:
             self.mysql_connection.execute_query("DROP TABLE IF EXISTS weeks")
             Base.metadata.tables["weeks"].create(self.engine)
+    
+    def create_week(self, date):
+        week = Weeks(
+            date = date,
+            tracker_order = [],
+            list_order = [[], [], []]
+        )
+        self.mysql_connection.session.add(week)
+        return self.get_week(date)
+    
+    def create_task(self, task, date, column):
+        week = self.mysql_connection.session.query(Weeks).filter_by(date=date).first()
+        self.mysql_connection.session.begin()
+        task = Tasks(
+            task=task,
+            status=0,
+            week_id=week.id
+        )
+        self.mysql_connection.session.add(task)
+        self.mysql_connection.session.commit()
+        a = copy.deepcopy(week.list_order[:])
+        while column > len(a) - 1:
+            a.append([])
+        a[column].append(task.id)
+        self.edit_week(date=week.date, list_order=a)
+        self.get_week(week.date)
+        return self.get_task(task.id)
+
+
+
+    def get_week(self, date):
+        week = self.mysql_connection.session.query(Weeks).filter_by(date=date).first()
+        if week:
+            self.mysql_connection.session.expire_all()
+            return {"id": week.id, "date": week.date, "tracker_order": week.tracker_order, "list_order": week.list_order}
+        
+    def get_task(self, task_id):
+        task = self.mysql_connection.session.query(Tasks).filter_by(id=task_id).first()
+        if task:
+            self.mysql_connection.session.expire_all()
+            return {"id": task.id, "task": task.task, "status": task.status, "days": task.days, "week_id": task.week_id}
+        else:
+            raise UserNotFoundException("Task not found")
+
+    def edit_task(self, task_id, task_text=None, status=None, days=None):
+        task = self.mysql_connection.session.query(Tasks).filter_by(id=task_id).first()
+        if task:
+            if not(task_text is None) and task.task != task_text:
+                task.task = task_text
+            if not(status is None) and task.status != status:
+                task.status = status
+            if not(days is None) and task.days != days:
+                task.days = days[:]
+            return self.get_task(task_id=task_id)
+        raise UserNotFoundException("Task not found")
+    
+
+    def edit_week(self, date, tracker_order=None, list_order=None):
+        week = self.mysql_connection.session.query(Weeks).filter_by(date=date).first()
+        if week:
+            if not(tracker_order is None) and week.tracker_order != tracker_order:
+                week.tracker_order = tracker_order
+            if not(list_order is None):
+                week.list_order = copy.deepcopy(list_order)
+            return self.get_week(date)
+        else:
+            raise UserNotFoundException("Week not found")
+    
+    def filter_task_for_week_id(self, date):
+        week = self.mysql_connection.session.query(Weeks).filter_by(date=date).first()
+        tasks = self.mysql_connection.session.query(Tasks).filter_by(week_id=week.id).all()
+        return tasks
--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 import sys
 import os
 # sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from flask import Flask
+from flask import Flask, request
 import json
 import threading
 import argparse
 # from flask_cors import CORS
-import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from DB.client.client import MySQLConnection
 from DB.interaction.interaction import DbInteraction
@@ -22,9 +21,122 @@ class Server:
         db_name="6ocra3$planner_db",
         rebuild_db=True
         )
+        self.app.add_url_rule("/shutdown", view_func=self.shutdown)
         self.app.add_url_rule("/", view_func=self.hello_world)
-
+        self.app.add_url_rule("/home", view_func=self.get_home)
+        self.app.add_url_rule("/create_task", view_func=self.create_task, methods=["POST"] )
+        self.app.add_url_rule("/create_week", view_func=self.create_week, methods=["POST"] )
+        self.app.add_url_rule("/edit_task", view_func=self.edit_task, methods=["PUT"] )
+        self.app.add_url_rule("/edit_week", view_func=self.edit_week, methods=["PUT"] )
+        self.app.add_url_rule("/get_week_tasks/<date>", view_func=self.get_task_for_week)
+        self.app.add_url_rule("/get_task/<task_id>", view_func=self.get_task)
+        self.app.add_url_rule("/get_week/<date>", view_func=self.get_week)
+        self.app.add_url_rule("/edit_task_status", view_func=self.edit_task_status, methods=["PUT"])
+        self.app.add_url_rule("/edit_task_day", view_func=self.edit_task_day, methods=["PUT"])
+    
     def hello_world(self):
-        return {"id": 7}, 200 
+        return "LET'S GO!!!"
+
+    def edit_task_status(self):
+        request_body = dict(request.json)
+        task_id = request_body["task_id"]
+        status = request_body["status"]
+        res = self.db.edit_task(task_id=task_id, status=status)
+        if res:
+            return "Succes", 200
+        
+    def edit_task_day(self):
+        request_body = dict(request.json)
+        task_id = request_body["task_id"]
+        day = request_body["day"]
+        value = request_body["value"]
+        task = self.db.mysql_connection.session.query(Tasks).filter_by(id=task_id).first()
+        new_days = task.days[:]
+        new_days[day] = value
+        res = self.db.edit_task(task_id=task_id, days=new_days)
+        if res:
+            return "Succes", 200
+
+    def get_task(self, task_id):
+        task = self.db.get_task(task_id=task_id)
+        return task, 200
+
+    def get_week(self, date):
+        week = self.db.get_week(date=date)
+        return week
+
+    def run_server(self):
+        self.server = threading.Thread(target=self.app.run, kwargs={"host": self.host, "port": self.port})
+        self.server.start()
+        return self.server
+
+    def shutdown_server(self):
+        request.get(f'http://{self.host}:{self.port}/shutdown')
+
+    def shutdown(self):
+        terminate_func = request.environ.get("werkzeuq.server.shutdown")
+        if terminate_func:
+            terminate_func()
+
+    def get_home(self):
+        return "Hello world!"
+    
+    def create_task(self):
+        request_body = dict(request.json)
+        task = request_body["task"]
+        date = request_body["date"]
+        column = request_body["column"]
+        newTask = self.db.create_task(task=task, date=date, column=column)
+        return {"id": newTask["id"]}, 201
+    
+    def create_week(self):
+        request_body = dict(request.json)
+        date = request_body["date"]
+        self.db.create_week(date=date)
+        return f"Succes add week with date {date}", 201
+    
+    def edit_task(self):
+        request_body = dict(request.json)
+        rkeys = list(request_body.keys())
+        task_text, status, days = None, None, None
+        if "task_id" in rkeys:
+            task_id = request_body["task_id"]
+        if "task_text" in rkeys:
+            task_text = request_body["task_text"]
+        if "status" in rkeys:
+            status = request_body["status"]
+        if "days" in rkeys:
+            days = request_body["days"]
+        res = self.db.edit_task(task_id=task_id, task_text=task_text, status=status, days=days)
+        if res:
+            return f"Succes edit {task_text}", 202
+    
+    def edit_week(self):
+        request_body = dict(request.json)
+        rkeys = list(request_body.keys())
+        date = request_body["date"]
+        tracker_order, list_order = None, None
+        if "tracker_order" in rkeys:
+            tracker_order = request_body["tracker_order"]
+        if "list_order" in rkeys:
+            list_order = request_body["list_order"]
+        res = self.db.edit_week(date=date, tracker_order=tracker_order, list_order=list_order)
+        if res:
+            return f"Succes edit week {date}", 202
+    
+        
+    def get_task_for_week(self, date):
+        week = self.db.get_week(date=date)
+        if not(week):
+            self.db.create_week(date=date)
+        tasks = self.db.filter_task_for_week_id(date=date)
+        ans = dict()
+        for i in tasks:
+            temp = { "task":i.task, 
+                     "status":i.status,
+                     "days": i.days}
+            ans[i.id] = temp
+        ans = json.dumps(ans)
+        return ans, 200
 server = Server()
 app = server.app
